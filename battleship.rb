1a4fac292dbbb431ad2600db65e53da6bbffcd4f
Dan Kleiman <info@dankleiman.com>
2014-06-28 11:54:18 -0400

    Sink a ship

--- battleship.rb
+++ battleship.rb
@@ -1,8 +1,8 @@
-# require_relative 'board'
 require 'pry'
 
-# board = Board.new
-# binding.pry
+##############
+# DRAW BOARD #
+##############
 
 
 board = []
@@ -14,40 +14,15 @@ board = []
   board << row
 end
 
-########################
-# HORIZONTAL PLACEMENT #
-########################
-
-# ship = "A"
-
-# i = 0
-# while i < 5
-#   board[0][i] = ship
-#   i += 1
-# end
-
-######################
-# VERTICAL PLACEMENT #
-######################
-
-# ship = "B"
-
-# i = 1
-
-# while i < 5
-#   board[i][0] = ship
-#   i += 1
-# end
-
-# puts "Vertical placement"
-# puts board.inspect
-
 ###################
 # PLACE ALL SHIPS #
 ###################
 
-ships = {"A" => [], "B" => [], "C" => [], "D" => [], "S" => [] }
-size = {"A" => 5, "B" => 4, "C" => 3, "D" => 2, "S" => 1 }
+ships = {"A" => [], "B" => [], "C" => [], "D" => [] }
+size = {"A" => 5, "B" => 4, "C" => 3, "D" => 2 }
+
+ships = {"A" => []}
+size = {"A" => 5}
 
 ships.each do |ship, position|
   start = []
@@ -73,33 +48,44 @@ ships.each do |ship, position|
       i += 1
     end
   end
-  puts "Ships: #{ships.inspect}"
+  puts "Position of Ship #{ship}: #{position}"
 end
-puts "Board: #{board.inspect}"
-
-
 
 ###############
 # FIRE A SHOT #
 ###############
 
-shot = [0,0]
-print "Fire a shot (row): "
-shot[0] = gets.chomp.to_i
-print "Fire a shot (column): "
-shot[1] = gets.chomp.to_i
-#factor in array counting vs column display?
+until ships.empty?
+  shot = []
+  print "Fire a shot (row): "
+  shot[0] = gets.chomp.to_i
+  print "Fire a shot (column): "
+  shot[1] = gets.chomp.to_i
+  #factor in array counting vs column display?
 
-if board[shot[0]][shot[1]] == "O"
-  puts "Miss!"
-else
-  board[shot[0]][shot[1]] = "H"
-  puts "Hit"
+  if board[shot[0]][shot[1]] == "O"
+    puts "Miss!"
+  else
+    board[shot[0]][shot[1]] = "H"
+    puts "Hit"
+  end
+  ships.each do |ship, position|
+    hits = 0
+    position.each do |p|
+      row = p[0]
+      col = p[1]
+      if board[row][col] == 'H'
+        hits += 1
+        puts board.inspect
+      end
+    end
+    if hits == position.length
+      puts "You sunk my #{ship} Ship!"
+      ships.delete(ship)
+    end
+  end
 end
 
-puts board.inspect
 
-###############
-# SINK A SHIP #
-###############
+
 
