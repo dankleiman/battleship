45f9cfc8e5b90ad1a7f1b40aa4b1a7b68dbe9c3d
Dan Kleiman <info@dankleiman.com>
2014-06-28 12:40:18 -0400

    Refactored hit miss for single player

--- battleship.rb
+++ battleship.rb
@@ -42,61 +42,51 @@ def place_ships(player, board, ships, size)
         i +=1
       end
     end
-    puts "Position of #{player} #{ship}: #{position}"
   end
-
 end
 
 # ships = {"Aircraft Carrier" => [], "Battleship" => [], "Cruiser" => [], "Destroyer" => [] }
 # size = {"Aircraft Carrier" => 5, "Battleship" => 4, "Cruiser" => 3, "Destroyer" => 2 }
 
-
 size = {"Aircraft Carrier" => 5}
 my_ships = {"Aircraft Carrier" => []}
-enemy_ships = {"Aircraft Carrier" => []}
+# enemy_ships = {"Aircraft Carrier" => []}
 
 my_board = new_board
-enemy_board = new_board
+# enemy_board = new_board
 
 place_ships("Player 1", my_board, my_ships, size)
-place_ships("Enemy", enemy_board, enemy_ships, size)
-
-binding.pry
+# place_ships("Enemy", enemy_board, enemy_ships, size)
 
 ########################################
 # FIRE A SHOTS UNTIL ALL SHIPS GO DOWN #
 ########################################
-
-until ships.empty?
+puts my_board.inspect
+until my_ships.empty?
   shot = []
   print "Fire a shot (row): "
   shot[0] = gets.chomp.to_i
   print "Fire a shot (column): "
   shot[1] = gets.chomp.to_i
 
-  if board[shot[0]][shot[1]] == "O"
+  if my_board[shot[0]][shot[1]] == "O"
     puts "Miss!"
   else
-    board[shot[0]][shot[1]] = "H"
+    my_board[shot[0]][shot[1]] = "H"
     puts "Hit!"
-  end
-  ships.each do |ship, position|
-    hits = 0
-    position.each do |p|
-      row = p[0]
-      col = p[1]
-      if board[row][col] == 'H'
-        hits += 1
-        puts board.inspect
+    my_ships.each do |ship, position|
+      hits = 0
+      position.each do |p|
+        row = p[0]
+        col = p[1]
+        if my_board[row][col] == 'H'
+          hits += 1
+        end
       end
-    end
-    if hits == position.length
-      puts "You sunk my #{ship}!"
-      ships.delete(ship)
+      if hits == position.length
+        puts "You sunk my #{ship}!"
+        my_ships.delete(ship)
+      end
     end
   end
 end
-
-
-
-
