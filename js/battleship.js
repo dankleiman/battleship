be5317a9853d67467948c2dfa2f9be2672e59b45
Dan Kleiman <info@dankleiman.com>
2014-06-28 18:51:02 -0400

    Place all the ships

--- js/battleship.js
+++ js/battleship.js
@@ -1,32 +1,38 @@
-// var ships = {
-//   'Aircraft Carrier': [],
-//   'Battleship': [],
-//   'Cruiser': [],
-//   'Destroyer': []
-// };
-// var size = {"Aircraft Carrier" => 5, "Battleship" => 4, "Cruiser" => 3, "Destroyer" => 2 };
+
 
 //Record position of each ship -- addClass('ship-' + currentShip) to each cell
-function placeShips(ship, size) {
-  var start = [];
-  var row = prompt("Starting Row for "+ship+" (0-9):");
-  start[0] = row;
-  var col = prompt("Starting Column for "+ship+" (0-9):");
-  start[1] = col;
-  var orientation = prompt("Orientation? (h for horizontal, v for vertical):").toLowerCase();
-  for (var i = 0; i < size; i++) {
-    if (orientation === 'h'){
-      col ++
+function placeShips(ships, size) {
+  for (var i = 0; i < ships.length; i++) {
+    var ship = ships[i]
+    var size = sizes[i]
+    var start = [];
+    var row = prompt("Starting Row for "+ship+" (0-9):");
+    start[0] = row;
+    var col = prompt("Starting Column for "+ship+" (0-9):");
+    start[1] = col;
+    var orientation = prompt("Orientation? (h for horizontal, v for vertical):").toLowerCase();
+
+    for (var j = 0; j < size; j++) {
       $(".row:eq("+row+") > .square:eq("+col+")").addClass('ship')
-      console.log(row, col)
+      if (orientation === 'h'){
+        col ++
+      } else{
+        row ++
+      }
     }
   }
+};
 
-  };
+var ships = [
+  'Aircraft Carrier',
+  'Battleship',
+  'Cruiser',
+  'Destroyer'
+  ];
 
+var sizes = [5, 4, 3, 2];
 
-
-placeShips('Aircraft Carrier', 5);
+placeShips(ships, sizes);
 
 //Fire a shot
 $('.board').on('click', '.square', function(event) {
