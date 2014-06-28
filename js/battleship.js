3706f590c09c4bbfcc2fe20f3aa0eb6563dc5d9e
Dan Kleiman <info@dankleiman.com>
2014-06-28 18:30:27 -0400

    Placed one ship on the js board

--- js/battleship.js
+++ js/battleship.js
@@ -9,13 +9,22 @@
 //Record position of each ship -- addClass('ship-' + currentShip) to each cell
 function placeShips(ship, size) {
   var start = [];
-  var row = prompt("Starting Row for "+ship+" (1-10):");
+  var row = prompt("Starting Row for "+ship+" (0-9):");
   start[0] = row;
-  var col = prompt("Starting Column for "+ship+" (1-10):");
+  var col = prompt("Starting Column for "+ship+" (0-9):");
   start[1] = col;
   var orientation = prompt("Orientation? (h for horizontal, v for vertical):").toLowerCase();
+  for (var i = 0; i < size; i++) {
+    if (orientation === 'h'){
+      col ++
+      $(".row:eq("+row+") > .square:eq("+col+")").addClass('ship')
+      console.log(row, col)
+    }
+  }
 
-}
+  };
+
+
 
 placeShips('Aircraft Carrier', 5);
 
