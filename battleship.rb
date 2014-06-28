bddaf99b2a66731449e27d0219a9cb5bff29ebcb
Dan Kleiman <info@dankleiman.com>
2014-06-28 11:18:18 -0400

    Place all ships on board.

--- battleship.rb
+++ battleship.rb
@@ -1,5 +1,5 @@
 # require_relative 'board'
-# require 'pry'
+require 'pry'
 
 # board = Board.new
 # binding.pry
@@ -18,29 +18,62 @@ end
 # HORIZONTAL PLACEMENT #
 ########################
 
-ship = "A"
+# ship = "A"
 
-i = 0
-while i < 5
-  board[0][i] = ship
-  i += 1
-end
+# i = 0
+# while i < 5
+#   board[0][i] = ship
+#   i += 1
+# end
 
 ######################
 # VERTICAL PLACEMENT #
 ######################
 
-ship = "B"
+# ship = "B"
 
-i = 1
+# i = 1
 
-while i < 5
-  board[i][0] = ship
-  i += 1
+# while i < 5
+#   board[i][0] = ship
+#   i += 1
+# end
+
+# puts "Vertical placement"
+# puts board.inspect
+
+###################
+# PLACE ALL SHIPS #
+###################
+
+ships = {"A" => [], "B" => [], "C" => [], "D" => [], "S" => [] }
+size = {"A" => 5, "B" => 4, "C" => 3, "D" => 2, "S" => 1 }
+
+ships.each do |ship, position|
+  start = []
+  print "Starting position for ship #{ship} (row): "
+  start[0] = gets.chomp.to_i
+  print "Starting position for ship #{ship} (column): "
+  start[1] = gets.chomp.to_i
+  print "Orientation? (h for horizontal, v for vertical): "
+  orientation = gets.chomp.downcase
+
+  i = start[0]
+  j = start[1]
+  if orientation == 'h'
+    while j < size[ship]
+      board[i][j] = ship
+      j +=1
+    end
+  else
+     while i < size[ship]
+      board[i][j] = ship
+      i += 1
+    end
+  end
+  puts board.inspect
 end
 
-puts "Vertical placement"
-puts board.inspect
 
 ###############
 # FIRE A SHOT #
